d261a107aa288ca55b1f312d550468b0b69e2cbc
Oskar Eriksson <oskar.eriksson9@gmail.com>
2015-08-11 11:18:39 +0200

    Added basic key handler

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,33 @@
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane Uass";
-//document.body.innerHTML = greeter(user);
+var Direction;
+(function (Direction) {
+    Direction[Direction["UP"] = 0] = "UP";
+    Direction[Direction["RIGHT"] = 1] = "RIGHT";
+    Direction[Direction["DOWN"] = 2] = "DOWN";
+    Direction[Direction["LEFT"] = 3] = "LEFT";
+})(Direction || (Direction = {}));
+var Player = (function () {
+    function Player(nick) {
+        this.nick = nick;
+    }
+    Player.prototype.move = function (direction) {
+        console.log(this.nick + ' moving ' + Direction[direction]);
+    };
+    return Player;
+})();
+var player = new Player('Jeppe');
+document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 38:
+            player.move(Direction.UP);
+            break;
+        case 39:
+            player.move(Direction.RIGHT);
+            break;
+        case 40:
+            player.move(Direction.DOWN);
+            break;
+        case 37:
+            player.move(Direction.LEFT);
+            break;
+    }
+};
--- src/application.ts
+++ src/application.ts
@@ -1,7 +1,37 @@
-function greeter(person) {
-    return "Hello, " + person;
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
 }
 
-var user = "Jane Uass";
+class Player {
+    nick: string;
+    constructor(nick: string) {
+        this.nick = nick;
+    }
 
-//document.body.innerHTML = greeter(user);
+    move(direction: Direction) {
+      console.log(this.nick + ' moving ' + Direction[direction]);
+    }
+}
+
+var player = new Player('Jeppe');
+
+document.onkeydown = function (e) {
+    switch(e.keyCode) {
+      case 38:
+        player.move(Direction.UP);
+      break;
+      case 39:
+        player.move(Direction.RIGHT);
+      break;
+      case 40:
+        player.move(Direction.DOWN);
+      break;
+      case 37:
+        player.move(Direction.LEFT);
+      break;
+    }
+
+};
