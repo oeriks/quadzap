16167edec15940190a8e8503781bf3417edff495
Oskar Eriksson <oskar.eriksson9@gmail.com>
2015-08-11 10:29:27 +0200

    Typescript yeahh

--- gruntfile.js
+++ gruntfile.js
@@ -3,14 +3,31 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          'public/stylesheets/application.css': 'sass/application.scss'
+          'public/stylesheets/application.css': 'src/application.scss'
+        }
+      }
+    },
+    typescript: {
+      base: {
+        src: ['src/**/*.ts'],
+        dest: 'public/javascripts/application.js',
+        options: {
+          module: 'amd',
+          target: 'es5'
         }
       }
     },
     watch: {
-      source: {
-        files: ['sass/**/*.scss', 'views/**/*.jade'],
+      sass: {
+        files: ['src/**/*.scss', 'views/**/*.jade'],
         tasks: ['sass'],
+        options: {
+          livereload: true, // needed to run LiveReload
+        }
+      },
+      typescript: {
+        files: ['src/**/*.ts'],
+        tasks: ['typescript'],
         options: {
           livereload: true, // needed to run LiveReload
         }
@@ -19,6 +36,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-sass');
-  grunt.registerTask('default', ['sass']);
+  grunt.loadNpmTasks('grunt-typescript');
+  grunt.registerTask('default', ['sass', 'typescript']);
   grunt.loadNpmTasks('grunt-contrib-watch');
 };
---
+++ public/javascripts/application.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane Uass";
+//document.body.innerHTML = greeter(user);
